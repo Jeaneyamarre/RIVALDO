a5a602fd326989defe67713a177cd5b81e575c15
Jeaneyamarre <61547378+Jeaneyamarre@users.noreply.github.com>
2021-04-13 16:40:38 +0200

    Delete models directory

--- models/music.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const musicSchema = mongoose.Schema({
-    title: { type: String, required: true },
-    author: { type: String, required: true },
-});
-
-module.exports = mongoose.model('Music', musicSchema);
